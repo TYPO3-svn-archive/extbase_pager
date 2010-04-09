3d616fda5d485e3d9d96fa9119a51f51e97e156c
nils.blattner <nils.blattner@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-09 11:40:20 +0000

    Added JavaScript for the pager (forgot on first commit)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_pager/trunk@32081 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Resources/Public/JavaScript/pager.js
@@ -0,0 +1,35 @@
+/**
+ * Handles the keyup event
+ *
+ * @var Event event The event.
+ * @var Dom el The Dom-Element.
+ * @var int lower The lower boundry.
+ * @var int upper The upper boundry.
+ * @return void
+ */
+function pagerJSkeyup(event, el, lower, upper) {
+	var form = el.up('form');
+	var value = pagerParseInt_(el.value);
+	if (value < lower) {
+		value = lower;
+	} else if (value > upper) {
+		value = upper;
+	}
+	el.value = value;
+}
+
+/**
+ * Alternate parseInt to make sure 001 is parsed as decimal and no NaN is returned.
+ *
+ * @param string text Text to parse as int.
+ * @return int Integer value of the text.
+ */
+function pagerParseInt_(text) {
+	text = text.replace(/^(0+)[1-9]/, '');
+	if (text == null || text == '' || isNaN(text)) return 0;
+	
+	var cleared = parseInt(text);
+	if (cleared < 0) return 0;
+	return cleared;
+}
+
